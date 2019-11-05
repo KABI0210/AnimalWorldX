9144148eedf03f7a4b2f2d802e52f4d5e1cd040f
kabi0210 <1713464909@qq.com>
2019-11-06 00:41:17 +0800

    摸鱼
    1、修改了宝箱01的双眼斜率。
    2、为其添加第一阶段的ai。

--- src/main/java/com/github/kabi0210/animalworldx/client/model/ModelPetBox001.java
+++ src/main/java/com/github/kabi0210/animalworldx/client/model/ModelPetBox001.java
@@ -75,5 +75,7 @@ public class ModelPetBox001 extends ModelBase {
 
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        this.e4.rotateAngleZ=0.5f;
+        this.e5.rotateAngleZ=0.5f;
     }
 }
